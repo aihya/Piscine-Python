634b5e141f8d1995e3a9402c74cedd578366bafa
Abdechahid Ihya <aihya@e1r6p12.1337.ma>
2022-03-26 16:35:02 +0100

    -

--- module02/ex03/csvreader.py
+++ module02/ex03/csvreader.py
@@ -1,20 +1,33 @@
 class CsvReader:
-    def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
+    def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=1):
         self.filename = filename
         self.sep = sep
         self.header = header
         self.skip_top = skip_top
         self.skip_bottom = skip_bottom
         self.data = []
+        self.head = None
 
     def __enter__(self):
-        self.file = open(self.filename, 'r')
-        for record in self.file.read().split('\n'):
-            fields = record.split(self.sep)
-            if self.data and len(fields) != len(self.data[0]):
+        try:
+            self.file = open(self.filename, 'r')
+        except FileNotFoundError:
+            return None
+
+        lines = list(filter(len, self.file.read().split('\n')))
+        records = [[field.strip() for field in line.split(self.sep)] for line in lines]
+
+        for record in records:
+            record = list(filter(len, record))
+            if len(record) != len(records[0]):
                 return None
-            print(fields)
-            self.data.append(fields)
+
+        if self.header:
+            self.head = records[0]
+            self.data = records[1:]
+        else:
+            self.data = records
+
         return self
 
     def __exit__(self, __type, __value, __traceback):
@@ -25,7 +38,7 @@ class CsvReader:
         Returns:
         nested list (list(list, list, ...)) representing the data.
         """
-        return self.data[self.skip_top + (1 if self.header else 0):self.skip_bottom]
+        return self.data[self.skip_top: len(self.data) - self.skip_bottom]
 
     def getheader(self):
         """ Retrieves the header from csv file.
@@ -33,12 +46,15 @@ class CsvReader:
         list: representing the data (when self.header is True).
         None: (when self.header is False).
         """
-        return self.header
+        return self.head
 
 
 if __name__ == "__main__":
-    with CsvReader('good.csv') as file:
-        data = file.getdata()
-        header = file.getheader()
-        print(data)
-        print(header)
+    with CsvReader('good.csv', header=True) as file:
+        if file is None:
+            print("File is corrupted")
+        else:
+            data = file.getdata()
+            header = file.getheader()
+            print(data)
+            print(header)
